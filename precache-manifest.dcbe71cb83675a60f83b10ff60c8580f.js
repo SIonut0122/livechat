090d6006ca2ef1788b4e2ad101e175028f037330
SIonut0122 <sionut0122@yahoo.com>
2019-10-09 23:53:10 +0100

    Updates

---
+++ precache-manifest.dcbe71cb83675a60f83b10ff60c8580f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f8698bea2d09b45b2d131b76dd7e6f",
+    "url": "/livechat/index.html"
+  },
+  {
+    "revision": "2c632a0da6db64ce63ea",
+    "url": "/livechat/static/css/main.8b33c88f.chunk.css"
+  },
+  {
+    "revision": "5323283346297d5078ad",
+    "url": "/livechat/static/js/2.f29421d0.chunk.js"
+  },
+  {
+    "revision": "2c632a0da6db64ce63ea",
+    "url": "/livechat/static/js/main.e148d54b.chunk.js"
+  },
+  {
+    "revision": "5b08374a0a7d15c1fc9e",
+    "url": "/livechat/static/js/runtime~main.d3d556a9.js"
+  },
+  {
+    "revision": "31689a1c48887a9f30f7b7c3c04af3f4",
+    "url": "/livechat/static/media/chat_bg.31689a1c.jpg"
+  },
+  {
+    "revision": "d8ced7bec7f53af902533ab0ee15e4bb",
+    "url": "/livechat/static/media/intro_star_bg.d8ced7be.jpg"
+  }
+]);
